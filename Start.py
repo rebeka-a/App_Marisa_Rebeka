fd2559d42af615d73ac964eda6ed2677b09eb516
Rebeka Ammann <rbkmmnn@gmail.com>
2025-02-26 08:09:25 +0100

    E-mail

--- Start.py
+++ Start.py
@@ -4,6 +4,8 @@ import pandas as pd
 st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Marisa und Rebeka entwickelt.")
+st.write("Marisa von Fellenberg: vonfemar@students.zhaw.ch") 
+st.write("Rebeka Ammann: ammanre1@students.zhaw.ch")
 st.write("Durch diese App wird es ihnen möglich ihre Daten zu organisieren und abzurufen.")
 if st.button("Welt retten", icon="🌍") :
     st.write("Booooom! 🌎💥🔥")
